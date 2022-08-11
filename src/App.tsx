cffe655e5fe4141a2351b5073c8e8e1bf56a84b7
João V <silva.joao1@outlook.com.br>
2022-08-11 00:47:46 -0300

    feat: styled-components and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+
 export function App() {
   return (
-    <div>
-      <p>Coffee Delivery</p>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <h1>Coffee Delivery</h1>
+    </ThemeProvider>
   );
 }
